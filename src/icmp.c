112bbcc902eb4a08b7bf30eeefe410d39feb9231
David Day <david.day@utexas.edu>
2022-03-20 17:43:51 +0000

    fix checksum

--- src/icmp.c
+++ src/icmp.c
@@ -2,6 +2,7 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include "icmp.h"
+#include "in_cksum.h"
 
 icmp_echo *get_icmp_echo_msg(uint16_t id, uint16_t seqnum) {
     icmp_echo *msg = malloc(sizeof(icmp_echo));
@@ -9,10 +10,13 @@ icmp_echo *get_icmp_echo_msg(uint16_t id, uint16_t seqnum) {
     msg->header.code = 0;
     msg->header.id =  hton16(id);
     msg->header.seqnum = hton16(seqnum);
+    msg->header.cksum = 0;
 
     for (int i = 0; i < ICMP_ECHO_DATA_SZ; i++) {
         msg->data[ICMP_ECHO_DATA_OFFSET + i] = 0x30 + i; 
     }
+
+    msg->header.cksum = csum((uint16_t *) msg->data, sizeof(icmp_echo));
 
     return msg;
 }
