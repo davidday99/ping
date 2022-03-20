8178d5affab2c0848386e5c37e05449c0f7575c9
David Day <david.day@utexas.edu>
2022-03-20 17:44:44 +0000

    remove hton macros and use those already defined

--- inc/icmp.h
+++ inc/icmp.h
@@ -7,9 +7,6 @@
 #define ICMP_STATIC_ID 0xCC
 #define ICMP_ECHO_DATA_SZ 48
 #define ICMP_ECHO_DATA_OFFSET sizeof(icmp_echo_hdr)
-#define hton16(x) ((((x) & 0xFF) << 8) | (((x) & 0xFF00) >> 8))
-#define hton32(x) ((((x) & 0xFF) << 24) | (((x) & 0xFF00) << 8) |  \
-                    (((x) & 0xFF0000) >> 8) | (((x) & 0xFF000000) >> 24))
 
 typedef struct __attribute__ ((packed)) {
     uint8_t type;
