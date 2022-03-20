2282ce606f1760be0bb7e5c688933937c70d0a57
David Day <david.day@utexas.edu>
2022-03-20 05:34:39 +0000

    move declarations to header

--- inc/icmp.h
+++ inc/icmp.h
@@ -1,6 +1,31 @@
-#ifndef _ICMP_H_
+#ifndef _ICMP_H_ 
 #define _ICMP_H_
 
+#include <stdint.h>
 
+#define ICMP_TYPE_ECHO 8
+#define ICMP_STATIC_ID 0xCC
+#define ICMP_ECHO_DATA_SZ 48
+#define ICMP_ECHO_DATA_OFFSET sizeof(icmp_echo_hdr)
+#define hton16(x) ((((x) & 0xFF) << 8) | (((x) & 0xFF00) >> 8))
+#define hton32(x) ((((x) & 0xFF) << 24) | (((x) & 0xFF00) << 8) |  \
+                    (((x) & 0xFF0000) >> 8) | (((x) & 0xFF000000) >> 24))
+
+typedef struct __attribute__ ((packed)) {
+    uint8_t type;
+    uint8_t code;
+    uint16_t cksum;
+    uint16_t id;
+    uint16_t seqnum;
+} icmp_echo_hdr;
+
+typedef union icmp_echo {
+    icmp_echo_hdr header;
+    uint8_t data[sizeof(icmp_echo_hdr) + ICMP_ECHO_DATA_SZ];
+} icmp_echo;
+
+
+icmp_echo *get_icmp_echo_msg(uint16_t id, uint16_t seqnum);
+void free_icmp_echo(icmp_echo *p); 
 
 #endif /* _ICMP_H_ */
--- src/icmp.c
+++ src/icmp.c
@@ -1,11 +1,23 @@
 #include <stdint.h>
 #include <netinet/in.h>
+#include <stdlib.h>
+#include "icmp.h"
 
-typedef struct icmphdr {
-    uint8_t type;
-    uint8_t code;
-    uint8_t cksum;
-    uint32_t unused;
+icmp_echo *get_icmp_echo_msg(uint16_t id, uint16_t seqnum) {
+    icmp_echo *msg = malloc(sizeof(icmp_echo));
+    msg->header.type = ICMP_TYPE_ECHO;
+    msg->header.code = 0;
+    msg->header.id =  hton16(id);
+    msg->header.seqnum = hton16(seqnum);
 
-} icmphdr;
+    for (int i = 0; i < ICMP_ECHO_DATA_SZ; i++) {
+        msg->data[ICMP_ECHO_DATA_OFFSET + i] = 0x30 + i; 
+    }
+
+    return msg;
+}
+
+void free_icmp_echo(icmp_echo *p) {
+    free(p);
+}
 
