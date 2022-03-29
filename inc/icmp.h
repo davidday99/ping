490887c6593fd258be92b6e67d37cbe6bc28f3fd
David Day <david.day@utexas.edu>
2022-03-29 00:12:36 +0000

    remove dynamic allocation of icmp echo message

--- inc/icmp.h
+++ inc/icmp.h
@@ -4,11 +4,12 @@
 #include <stdint.h>
 
 #define ICMP_TYPE_ECHO 8
+#define ICMP_TYPE_ECHO_REPLY 0
 #define ICMP_STATIC_ID 0xCC
 #define ICMP_ECHO_DATA_SZ 48
 #define ICMP_ECHO_DATA_OFFSET sizeof(icmp_echo_hdr)
 
-typedef struct __attribute__ ((packed)) {
+typedef struct __attribute__ ((packed)) icmp_echo_hdr {
     uint8_t type;
     uint8_t code;
     uint16_t cksum;
@@ -22,7 +23,8 @@ typedef union icmp_echo {
 } icmp_echo;
 
 
-icmp_echo *get_icmp_echo_msg(uint16_t id, uint16_t seqnum);
-void free_icmp_echo(icmp_echo *p); 
+void init_icmp_echo(icmp_echo *msg, uint16_t id, uint16_t seqnum);
+char verify_icmp_echo_reply(icmp_echo *sent, icmp_echo *recv);
+void increment_icmp_echo_seqnum(icmp_echo *echo);
 
 #endif /* _ICMP_H_ */
