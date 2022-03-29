4413582f1d1b2ca854ea13508b983b1501e861dd
David Day <david.day@utexas.edu>
2022-03-29 00:14:10 +0000

    remove dynamic allocation of icmp echo message and recalculate checksum after incrementing seqnum

--- src/icmp.c
+++ src/icmp.c
@@ -1,15 +1,16 @@
 #include <stdint.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
+#include <string.h>
 #include "icmp.h"
 #include "in_cksum.h"
 
-icmp_echo *get_icmp_echo_msg(uint16_t id, uint16_t seqnum) {
-    icmp_echo *msg = malloc(sizeof(icmp_echo));
+void init_icmp_echo(icmp_echo *msg, uint16_t id, uint16_t seqnum) {
     msg->header.type = ICMP_TYPE_ECHO;
     msg->header.code = 0;
-    msg->header.id =  hton16(id);
-    msg->header.seqnum = hton16(seqnum);
+    msg->header.id =  htons(id);
+    msg->header.seqnum = htons(seqnum);
     msg->header.cksum = 0;
 
     for (int i = 0; i < ICMP_ECHO_DATA_SZ; i++) {
@@ -17,11 +18,25 @@ icmp_echo *get_icmp_echo_msg(uint16_t id, uint16_t seqnum) {
     }
 
     msg->header.cksum = csum((uint16_t *) msg->data, sizeof(icmp_echo));
-
-    return msg;
 }
 
-void free_icmp_echo(icmp_echo *p) {
-    free(p);
+char verify_icmp_echo_reply(icmp_echo *sent, icmp_echo *recv) {
+    if (sent->header.id != recv->header.id ||
+        sent->header.seqnum != recv->header.seqnum ||
+        recv->header.type != ICMP_TYPE_ECHO_REPLY ||
+        memcmp(&sent->data[ICMP_ECHO_DATA_OFFSET], 
+               &recv->data[ICMP_ECHO_DATA_OFFSET], 
+               ICMP_ECHO_DATA_SZ) != 0) {
+        return 0;
+    }
+    return 1;
+}
+
+void increment_icmp_echo_seqnum(icmp_echo *echo) {
+    uint16_t seqnum = ntohs(echo->header.seqnum);
+    seqnum++;
+    echo->header.seqnum = htons(seqnum);
+    echo->header.cksum = 0;
+    echo->header.cksum = csum((uint16_t *) echo->data, sizeof(icmp_echo));
 }
 
